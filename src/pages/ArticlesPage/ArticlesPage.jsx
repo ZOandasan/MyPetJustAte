448261c66ebe0f9dca48826dd84486a1984074a1
rei-bp <reinard.bp@gmail.com>
2021-09-22 22:44:07 -0700

    style articlepage

--- src/pages/ArticlesPage/ArticlesPage.jsx
+++ src/pages/ArticlesPage/ArticlesPage.jsx
@@ -4,8 +4,7 @@ import './ArticlesPage.css'
 export default function ArticlesPage() {
     return (
         <div className='articles-container'>
-            <p><span className="articles-title">Articles</span> <span className='articles-description'>Our Source Selection Criteria</span>
-            </p>
+            <div className='articles-title'>Learn More</div>
             <div id='rover' className='article-card'>
                 <div className='article-title'>50 Foods Dogs Can't Eat</div>
                 <div className='article-link'>Click Here to Read the Article</div>
@@ -14,6 +13,8 @@ export default function ArticlesPage() {
                 <div className='article-title'>Top 10 Best Foods For Dogs</div>
                 <div className='article-link'>Click Here to Read the Article</div>
             </div>
+
+            
         </div>
     )
 }
