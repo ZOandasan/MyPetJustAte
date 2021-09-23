947b40fccb2ccec755ac2204adadbdf10b6e8be7
ZOandasan <andsorez@gmail.com>
2021-09-23 13:45:19 -0700

    Styling Rush

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -13,56 +13,56 @@ export default function AboutPage(){
           </div>
           <div className='description'>Content Strategist / UX Designer</div>
         </div>
+
+        <div className="profile">
+          <div className="title">
+            <a href="https://www.linkedin.com/in/jimmy-liu-a9169310/">
+              <img className="linkedin" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/600px-LinkedIn_logo_initials.png" alt="linkedin"></img>
+            </a>
+            <div className='name'>Jimmy Liu</div>
+          </div>
+          <div className='description'>Content Strategist / UX Designer</div>
+        </div>
+
+        <div className="profile">
+          <div className="title">
+            <a href="https://www.linkedin.com/in/josiahwu/">
+              <img className="linkedin" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/600px-LinkedIn_logo_initials.png" alt="linkedin"></img>
+            </a>
+            <div className='name'>Josiah Wu</div>
+          </div>
+          <div className='description'>Content Strategist / UX Designer</div>
+        </div>
+
         
         <div className="profile">
           <div className="title">
             <a href="https://www.linkedin.com/in/carlos-shows/">
               <img className="linkedin" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/600px-LinkedIn_logo_initials.png" alt="linkedin"></img>
             </a>
-            <div className='name'>Carlos Shows</div>
+            <div className='name'>Natalie McCook</div>
           </div>
-          <div className='description'>Content Strategist / UX Designer</div>
+          <div className='description'>Full Stack Developer</div>
         </div>
 
         <div className="profile">
           <div className="title">
-            <a href="https://www.linkedin.com/in/carlos-shows/">
+            <a href="https://www.linkedin.com/in/reibp">
               <img className="linkedin" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/600px-LinkedIn_logo_initials.png" alt="linkedin"></img>
             </a>
-            <div className='name'>Carlos Shows</div>
+            <div className='name'>Rei Pagdilao</div>
           </div>
-          <div className='description'>Content Strategist / UX Designer</div>
-        </div>
-
-        
-        <div className="profile">
-          <div className="title">
-            <a href="https://www.linkedin.com/in/carlos-shows/">
-              <img className="linkedin" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/600px-LinkedIn_logo_initials.png" alt="linkedin"></img>
-            </a>
-            <div className='name'>Carlos Shows</div>
-          </div>
-          <div className='description'>Content Strategist / UX Designer</div>
+          <div className='description'>Full Stack Developer</div>
         </div>
 
         <div className="profile">
           <div className="title">
-            <a href="https://www.linkedin.com/in/carlos-shows/">
+            <a href="https://www.linkedin.com/in/zachary-oandasan/">
               <img className="linkedin" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/600px-LinkedIn_logo_initials.png" alt="linkedin"></img>
             </a>
-            <div className='name'>Carlos Shows</div>
+            <div className='name'>Zachary Oandasan</div>
           </div>
-          <div className='description'>Content Strategist / UX Designer</div>
-        </div>
-
-        <div className="profile">
-          <div className="title">
-            <a href="https://www.linkedin.com/in/carlos-shows/">
-              <img className="linkedin" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/600px-LinkedIn_logo_initials.png" alt="linkedin"></img>
-            </a>
-            <div className='name'>Carlos Shows</div>
-          </div>
-          <div className='description'>Content Strategist / UX Designer</div>
+          <div className='description'>Full Stack Developer</div>
         </div>
 
       </div>
