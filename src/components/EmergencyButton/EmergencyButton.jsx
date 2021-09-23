46a43375afcd0bf3c90b46d9360525e2704965ee
rei-bp <reinard.bp@gmail.com>
2021-09-23 10:39:07 -0700

    full match on home landing page

--- src/components/EmergencyButton/EmergencyButton.jsx
+++ src/components/EmergencyButton/EmergencyButton.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom"
+import './EmergencyButton.css'
 
 export default function EmergencyButton({setNavMenu}){
     return (
         <>
-            <Link to="/emergency"><button className="emergency" onClick={() => setNavMenu(true)}>Emergency</button></Link>
+            <Link to="/emergency"><button className="emergency" onClick={() => setNavMenu(true)}>Find a Vetinarian</button></Link>
         </>
     )
 }
